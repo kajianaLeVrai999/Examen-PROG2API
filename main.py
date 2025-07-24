e75435c2e41db22aaf801a8f5b0a65e23ab6fe1a
kajianaLeVrai999 <hei.kajiana@gmail.com>
2025-07-24 08:46:04 +0300

    G1 + G2

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from pydantic import BaseModel
+from starlette.requests import Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue sur FastAPI sous Windows"}
+
+@app.get("/hello")
+def read_hello():
+    return JSONResponse({"message": "Hello world"}, status_code=200)
+
+@app.get("/welcome")
+def read_welcome(request: Request, name: str = "Non défini"):
+    return JSONResponse({"message": f"Hello {name}"}, status_code=200)
